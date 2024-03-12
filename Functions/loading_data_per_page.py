6cab0c377d397fae79c3d94c3aac5c26e920314c
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-03-12 23:23:29 +0400

    Created loading_data_per_page.py

---
+++ Functions/loading_data_per_page.py
@@ -0,0 +1,14 @@
+import json
+
+def read_json_file(filename, page_number):
+    '''
+    Reads file and returns data by page (data is sorted by page)
+    :param filename: Name of file where is data
+    :param page: number of wanted page
+    :return: return json format data
+    '''
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    page = f'page{page_number}'
+    return d[page]
+
