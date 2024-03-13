bd1c3974669191e15210ca1eff74843b69b0b36a
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-03-13 11:24:06 +0400

    Finnished loading_data_per_page.py

--- Functions/loading_data_per_page.py
+++ Functions/loading_data_per_page.py
@@ -1,6 +1,7 @@
 import json
 
-def read_json_file(filename, page_number):
+
+def read_load_json_file(filename, page_number):
     '''
     Reads file and returns data by page (data is sorted by page)
     :param filename: Name of file where is data
@@ -10,5 +11,5 @@ def read_json_file(filename, page_number):
     with open(filename, 'r') as file:
         data = json.load(file)
     page = f'page{page_number}'
-    return d[page]
 
+    return data[page]
