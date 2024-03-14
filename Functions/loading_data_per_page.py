f436a62be193abd5e811569e540d36878df97c9e
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-03-14 09:32:52 +0400

    Fixed bugs in loading_data_per_page.py

--- Functions/loading_data_per_page.py
+++ Functions/loading_data_per_page.py
@@ -1,20 +1,30 @@
 import json
 
 
-def read_load_json_file(filename, page_number):
+def read_load_json_file(filename, page_numbers):
     '''
     Reads file and returns data by page (data is sorted by page)
     :param filename: Name of file where is data
-    :param page: number of wanted page
-    :return: return json format data
+    :param page_numbers: List of page numbers
+    :return: Return JSON format data
     '''
-    with open(filename, 'r') as file:
+    with open(filename, 'r',encoding='utf-8') as file:
         data = json.load(file)
-    return_data = []
-    if type(page_number) == list:
-        for p in page_number:
-            page = f'page{p}'
-            return_data.extend(data[page])
-        return return_data
+
+    return_data = {}
+
+    if isinstance(page_numbers, list):
+        for page_number in page_numbers:
+            page_key = f'page{page_number}'
+            if page_key in data:
+                return_data.update(data[page_key])
     else:
-        return data[f'page{page_number}']
+        page_key = f'page{page_numbers}'
+        if page_key in data:
+            return_data.update(data[page_key])
+
+    return return_data
+
+
+if __name__ == "__main__":
+   print(read_load_json_file(r'C:\Users\Mikayel\PycharmProjects\EnglishApp\Data\data.json', [1,2,3]))
