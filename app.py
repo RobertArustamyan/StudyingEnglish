bcd09ec586334887d745bd3006f95e767145cd96
Robert Arustamyan <robertarustamyan2@gmail.com>
2024-03-15 22:21:26 +0400

    Modified app.py

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ def get_new_word():
     return jsonify({"word": words[0], "translation": words[1]})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
 
